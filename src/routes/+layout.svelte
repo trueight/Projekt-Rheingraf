22b3677f8e0489c78c09c84cac9d79ac6c63c5b8
Hachi-R <drive8hachi@gmail.com>
2024-09-25 21:23:44 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { ModeWatcher } from "mode-watcher";
+	import '../app.css';
+ </script>
+	
+ <ModeWatcher />
+ <slot />
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import HeroiconsArrowUpOnSquare from '~icons/heroicons/arrow-up-on-square';
+ </script>
+  
+ <Button>
+  <HeroiconsArrowUpOnSquare/>
+  Share
+</Button>
+
